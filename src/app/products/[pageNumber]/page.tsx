b96fdc95337345f1500ad3a6bddee1f1c48075f9
Konrad Pawelec <konrad.pawelec@icloud.com>
2024-02-16 20:31:02 +0100

    Static pages geneation added

--- src/app/products/[pageNumber]/page.tsx
+++ src/app/products/[pageNumber]/page.tsx
@@ -1,6 +1,10 @@
 import { getProdcutsByPage } from "@/api/prodcuts";
 import { ProductList } from "@/components/oragnism /ProductList";
 
+export function generateStaticParams() {
+	return [{ pageNumber: "1" }, { pageNumber: "2" }, { pageNumber: "3" }, { pageNumber: "4" }];
+}
+
 export default async function ProductsPageNumber({ params }: { params: { pageNumber: string } }) {
 	const offset = (parseInt(params.pageNumber) - 1) * 20;
 	const products = await getProdcutsByPage(offset.toString());
